462cc59285d93f384601de7cd6940a09970fd901
1154933940 <1154933940@qq.com>
2020-07-18 09:03:35 +0800

    添加项目文件。

---
+++ DWMS.Web/Data/DefaultContext.cs
@@ -0,0 +1,18 @@
+﻿using DWMS.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DWMS.Web.Data
+{
+    public class DefaultContext:DbContext
+    {
+        public DefaultContext(DbContextOptions<DefaultContext> options)
+            :base(options)
+        { }
+
+        public DbSet<SysUser> SysUsers { get; set; }
+    }
+}
